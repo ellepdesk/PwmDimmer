105d6685a18bf16777ac913c5f15995da2919043
Pelle van der Heide <Pelle.van-der-Heide@nl.abb.com>
2014-11-07 17:17:29 +0100

    Enable interrupts

--- main.cpp
+++ main.cpp
@@ -18,8 +18,18 @@
  */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 #include "bitaccess.h"
 #include <util/delay_basic.h>
+
+//only for wake-up
+EMPTY_INTERRUPT(PCINT0_vect);
+ISR(TIM1_COMPA_vect)
+{
+    //stop timer
+    clrbit(GTCCR,0);
+}
 
 enum state
 {
@@ -33,26 +43,44 @@ static bool countUp = false;
 static uint16_t tick_counter = 0;
 static state s = OFF;
 
+void setupSleepTimer()
+{
+    TCCR1 = 0b0000111; // 1MHz/1024 about 1ms/tick
+    setbit(TIMSK,6);   // enable interrupt on ocr1a
+    setbit(GTCCR,1);   // start timer
+}
+
 void setupPwm()
 {
     //Set up timer for fast-pwm
     TCCR0A = 0b10000011;
     TCCR0B = 0b00000001;
-    GTCCR  = 0b00000001;
+    setbit(GTCCR,0);
 }
 
 void deSetupPwm()
 {
     //Clear timer registers
-    GTCCR  = 0b00000000;
+    clrbit(GTCCR,0);
     TCCR0A = 0b00000000;
     TCCR0B = 0b00000000;
 }
 
-void sleep()
+void sleep(uint8_t period = 0)
 {
-    const uint16_t delay = 0xFFF;
-    _delay_loop_2(delay);
+    if (period > 0)
+    {
+        OCR1A = period;
+        setupSleepTimer();
+    }
+    sleep_cpu();
+    //when value > 0
+    //  load timer 1 with value
+    //  start timer
+    //go to low-power mode
+    //will return on either button or timeout
+    //const uint16_t delay = 0xFFF;
+    //_delay_loop_2(delay);
 }
 
 bool pollButton()
@@ -61,7 +89,7 @@ bool pollButton()
     if (!readbit(PINB, PINB4))
     {
         //de-bounce
-        sleep();
+        sleep(100);
         if (!readbit(PINB, PINB4))
         {
             toReturn = true;
@@ -69,7 +97,7 @@ bool pollButton()
     }
     else
     {
-        sleep();
+        sleep(100);
     }
     return toReturn;
 }
@@ -88,8 +116,9 @@ void brightnessAdjust()
     }
 }
 
-void handleState(bool button)
+bool handleState(bool button)
 {
+    bool busy = false;
     switch (s)
     {
         case OFF:
@@ -97,19 +126,21 @@ void handleState(bool button)
         if (button)
         {
             tick_counter = 0;
+            busy = true;
             s = (s == ON) ? ON_BUTTON_PRESSED : OFF_BUTTON_PRESSED;
         }
         break;
 
         case OFF_BUTTON_PRESSED:
+        busy = true;
         tick_counter++;
         //long press -> on at MIN
-        if (tick_counter > 20)
+        if (tick_counter > 10)
         {
             OCR0A = 0x0;
             countUp = true;
             setupPwm();
-            s = ON;
+            s = ON_BUTTON_PRESSED;
         }
         //short press -> on
         if (! button)
@@ -120,8 +151,9 @@ void handleState(bool button)
         break;
 
         case ON_BUTTON_PRESSED:
+        busy = true;
         tick_counter++;
-        if (tick_counter < 20)
+        if (tick_counter < 10)
         {
             //short press -> off
             if (! button)
@@ -139,25 +171,38 @@ void handleState(bool button)
                 s = ON;
                 countUp = !countUp;
             }
-            else
+            else if (tick_counter > 20)
             {
                 brightnessAdjust();
             }
         }
         break;
     }
+    return busy;
 }
 
 int main(void)
 {
     setbit(DDRB,DDB0);
     setbit(DDRB,DDB3);
+
+    //enable pin-change interrupt in PB4, using internal pull-up
     setbit(PORTB,PORTB4);
+    setbit(GIMSK, 5);
+    setbit(PCMSK, PCINT4);
     OCR0A = 0xFF;
+    sei();
+    sleep_enable();
+    set_sleep_mode(SLEEP_MODE_IDLE);
     while (true)
     {
         bool button = pollButton();
-        handleState(button);
+        if (!handleState(button))
+        {
+            sleep(0);
+            //allow deep sleep
+            //wake up by pin change
+        }
     }
     return 0;
 }
